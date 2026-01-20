9fd2e1866661bb9130c1f59ee86baad8a9e513ac
karthikeyan <karthikeyan76652@gmail.com>
2026-01-20 19:54:06 +0530

    UI work started

--- app/src/main/java/com/example/xmllearning/MainActivity.kt
+++ app/src/main/java/com/example/xmllearning/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.example.xmllearning
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    fun showToast(value: String){
+        Toast.makeText(applicationContext, value, Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //edgeToEdge is draw behind the system bars
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        showToast("OnCreate")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showToast("OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showToast("OnResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showToast("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showToast("OnStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showToast("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showToast("OnDestroy")
     }
 }
